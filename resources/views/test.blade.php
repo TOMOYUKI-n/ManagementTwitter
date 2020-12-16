626856aac27ef16010450a0156b297d12cc9d1e2
tomoyuki <xxbgxxlk.tm@gmail.com>
2020-12-17 00:37:13 +0900

    [update]css変数化

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,48 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-
-
 <div class="l-main__container">
-    <div class="p-login__container">
+    <div class="p-verify__container">
+        <!-- タイトル -->
+        <div class="l-main__title">
+            {{ __('Verify Your Email Address') }}
+        </div>
 
-        <form method="POST">
-            @csrf
+        <!-- 内容 -->
+        <div class="p-login__inner">
             
-            <div class="p-send__section">
-                <label for="email" class="p-send__text">{{ __('E-Mail Address') }}</label>
-                <div class="p-send__inner">
-                    <input id="email" type="email" class="p-send__form @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-                    @error('email')
-                        <span class="c-alert__caution2">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+
+            <div class="p-verify__text">
+                <p>
+                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                </p>
             </div>
-            <div class="p-send__section">
-                <label for="password" class="p-send__text">{{ __('Password') }}</label>
-                <div class="p-send__inner">
-                    <input id="password" type="password" class="p-send__form @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                    @error('password')
-                        <span class="c-alert__caution2">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+            <div class="p-verify__here">
+                <a class="p-login__alarttext">
+                    {{ __('If you did not receive the email click here to request another') }}
+                </a>
             </div>
-            <div class="p-send__section">
-                <label for="password-confirm" class="p-send__text">{{ __('Confirm Password') }}</label>
-                <div class="p-send__inner">
-                    <input id="password-confirm" type="password" class="p-send__form" name="password_confirmation" required autocomplete="new-password">
-                </div>
-            </div>
-            <div class="p-login__button__wrap">
-                    <button type="submit" class="p-send__text p-send__button">
-                        {{ __('Reset Password') }}
-                    </button>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
+
 @endsection
